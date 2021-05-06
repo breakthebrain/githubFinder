7c686f61d3c53339561bdb19bf17c99ee0cf02c8
Alexandr Front <breakthebrain1@gmail.com>
2021-05-06 09:33:31 +0300

    github finder

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <header>
+      <div class="navbar">
+        <div class="container">
+          <div class="navbar-content">
+            <div class="logo">GitHub Finder</div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {
+          title: "Home",
+          url: "/",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+
+
+        <search 
+        :value="search"
+        placeholder="Type user name"
+        @search="search = $event"
+        />
+
+        <button class="btn btnPrimary" @click="getRepos">Search</button>
+
+
+        <div class="repos__wrapper" v-if="repos">
+          <div class="repo-item" v-for="repo in repos" :key="repo.id">
+            <div class="repos-info">
+              <a class="link" :href="repo.html_url" target="_blank">{{repo.name}}</a>
+              <span>{{repo.stargazers_count}} ⭐</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+import search from '@/components/Search.vue'
+import axios from 'axios'
+
+export default {
+  components: {search},
+  data() {
+    return {
+      search: '',
+      repos: null
+    }
+  },
+  methods: {
+    getRepos() {
+      axios
+        .get(`https://api.github.com/users/${this.search}/repos`)
+        .then(res => {
+          console.log(res)
+          this.repos = res.data
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+  }
+  button {
+    margin-top: 40px;
+  }
+
+  .repos__wrapper {
+    width: 600px;
+    margin: 30px 0;
+  }
+  .repos-info {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 10px;
+    padding: 10px 0;
+    border-bottom: 1px solid #dbdbdb; 
+  }
+</style>
+
